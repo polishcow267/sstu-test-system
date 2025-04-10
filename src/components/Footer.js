941fc1a9cc2bd75270b7413cf1a69537439b3447
vlsbd <91143641+vlsbd@users.noreply.github.com>
2025-04-10 03:10:51 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
     return (
         <footer>
-            <p>&copy; 2024 ТЕСТИНТЕЛ</p> <span>Версия: 11.1-DEV</span> <a href="/guid" className='info'>О проекте &#10004;</a>
+            <p>&copy; 2024 ТЕСТИНТЕЛ</p> <span>Версия: 12.5-DEV</span> <a href="/guid" className='info'>О проекте &#10004;</a>
         </footer>
     )
 }
