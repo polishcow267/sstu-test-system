aba64afea26c9f84399416b3c47211984168c8f2
Michael <domfrost78@gmail.com>
2026-04-25 09:36:45 +0400

    Add using of deviceId

--- src/components/setTokenStored.js
+++ src/components/setTokenStored.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { getOrCreateDeviceId } from 'deviceId';
+import { getOrCreateDeviceId } from './deviceId';
 
 export default function setAuthTokenStored() { //установка токена для авторизации всех запросов
     localStorage.getItem("accessToken") ? axios.defaults.headers.common["Authorization"] = `Bearer ` + localStorage.getItem("accessToken") : delete axios.defaults.headers.common["Authorization"];
